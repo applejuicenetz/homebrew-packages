c4ee2f96df8cc584fd74816d5abeedc43cacb1e1
red171 <red171@appledocs.to>
2020-04-15 14:00:16 +0200

    * add homebrew tap

---
+++ applejuice-core.rb
@@ -0,0 +1,46 @@
+class Java8Requirement < Requirement
+  fatal true
+
+  satisfy do
+    system "/usr/libexec/java_home -v 1.8"
+  end
+
+  def message; <<~EOS
+    Java 8 Runtime Environment is required; install it via:
+      brew tap AdoptOpenJDK/openjdk
+      brew cask install adoptopenjdk8-jre
+  EOS
+  end
+end
+
+class ApplejuiceCore < Formula
+  desc "appleJuice Core"
+  homepage "https://applejuicenet.de"
+  url "https://download.applejuicenet.org/snapshot/0.31/ajcore-149.jar"
+  sha256 "089800059f9645423da3a4e5c3f986c620cb78e80893b8b34dfa2ba64a79d7f6"
+  version "0.31.149.110"
+  depends_on Java8Requirement
+
+  resource "ajnetmask" do
+    url "https://github.com/appleJuiceNET/applejuice-libajnetmask/releases/download/1.0.0/libajnetmask.jnilib"
+    sha256 "0371f17d01ff8f2d6ca69fb61e5d77157dedb788aecd3a0aa22525696b78538f"
+  end
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"ajcore").write <<~EOS
+      #!/bin/sh
+      export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+      cd #{libexec} 
+      read -p "Enter Memory in MB [1024]: " RamInput
+      RAM=${RamInput:-1024}
+      set -x
+      java -jar -Xmx${RAM}m ajcore-149.jar "$@"
+    EOS
+
+    resource("ajnetmask").stage do
+      (libexec).install Dir["libajnetmask.jnilib"]
+    end
+  end
+
+end
---
+++ readme.md
@@ -0,0 +1,29 @@
+# appleJuice Homebrew Tap
+
+easy installation guide to install `ajcore` and `ajgui` on macOS
+
+# requirements
+
+- install Homebrew from https://brew.sh
+- install Java 8 JRE `brew cask install adoptopenjdk8-jre`
+
+## install 
+
+- `brew tap applejuicenet/packages` 
+- `brew install applejuice-core`
+- `brew install applejuice-gui`
+
+## uninstall
+
+- `brew uninstall applejuice-gui`
+- `brew uninstall applejuice-core`
+- `brew untap applejuicenet/packages` 
+
+
+## run
+
+- open the `Terminal` app
+- enter `ajcore` and press Enter
+
+- open a 2nd `Terminal` app
+- enter `ajgui` and press Enter
