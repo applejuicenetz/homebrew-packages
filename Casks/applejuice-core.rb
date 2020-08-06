813be322cee5395108c2cd7adb1c1954bd3424df
red171 <red171@appledocs.to>
2020-08-06 21:52:45 +0200

    core version bump to 0.31.149.111

--- Casks/applejuice-core.rb
+++ Casks/applejuice-core.rb
@@ -1,11 +1,10 @@
 cask 'applejuice-core' do
-  version '0.31.149.110'
-  sha256 '270539fa1d1067860beb6fc3efcfaea1b26587c5972045656915ec0490501705'
+  version '0.31.149.111'
+  sha256 '8f8d152bb2e23da22361d395a2515291ffed7de8a935bea18431aaa171246ceb'
   url "https://github.com/appleJuiceNET/core/releases/download/#{version}/AJCore.macOS.zip"
   name 'AjCore'
   homepage 'https://applejuicenet.de'
   depends_on arch: :intel
-  depends_on cask: 'adoptopenjdk/openjdk/adoptopenjdk8-jre'
 
   auto_updates true
 
