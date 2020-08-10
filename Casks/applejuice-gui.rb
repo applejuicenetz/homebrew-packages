b3c23b15fd1e0bbb1725ba32b65806c598b92dbd
red171 <red171@appledocs.to>
2020-08-10 14:07:58 +0200

    update gui to 0.82.1

--- Casks/applejuice-gui.rb
+++ Casks/applejuice-gui.rb
@@ -1,6 +1,6 @@
 cask 'applejuice-gui' do
-  version '0.82.0'
-  sha256 'c31181682f6331f516f61991f71c9610264c96f1fda495c1c925f8ae88f571e9'
+  version '0.82.1'
+  sha256 'ed6556ab5b18df79f738f33a2c3e2ea672b627568588ddd3340f96ac37811535'
   url "https://github.com/appleJuiceNET/gui-java/releases/download/#{version}/AJCoreGUI.macOS.zip"
   name 'AJCoreGUI'
   homepage 'https://applejuicenet.de'
