c332c74e45c0d9e0553d0b571439b19003f7327f
red171 <red171@users.noreply.github.com>
2020-11-17 15:57:38 +0000

    Update Core to

--- Casks/applejuice-core.rb
+++ Casks/applejuice-core.rb
@@ -1,9 +1,11 @@
 cask 'applejuice-core' do
-  version '0.31.149.111'
-  sha256 '8f8d152bb2e23da22361d395a2515291ffed7de8a935bea18431aaa171246ceb'
+  version '0.31.149.112'
+  sha256 '777ea56ec1fcec78ba79edca75ea3d71b7990f5ef441c385bc3cdf9f326af7c1'
   url "https://github.com/applejuicenetz/core/releases/download/#{version}/AJCore.macOS.zip"
   name 'AjCore'
   homepage 'https://applejuicenet.de'
+  license 'proprietary'
+  depends_on arch: :intel
 
   auto_updates true
 
